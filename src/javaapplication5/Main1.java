0d304539f4d9abada42204ea807cf1c1ccb2be0f
Victor <victordiaznombela@gmail.com>
2021-02-01 09:39:08 +0100

    Ejercicios 1 y 2

---
+++ src/javaapplication5/Main1.java
@@ -0,0 +1,30 @@
+package javaapplication5;
+
+/**
+ *
+ * 
+ */
+public class Main1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        //De cadena a array
+        
+        String sCadena = "Esto es una cadena de texto";
+        char[] aCaracteres = sCadena.toCharArray();
+        
+        
+        //Resultado
+
+        System.out.println(aCaracteres);
+        
+        
+        
+    }
+    
+    
+}
